648067276ba8f0907ff110f639a8f29d121decec
ClaireBvs <clairembeauvais@gmail.com>
2018-09-12 09:49:37 -0600

    add gem faraday and id and key with figaro

--- app/presenter/search_presenter.rb
+++ app/presenter/search_presenter.rb
@@ -6,7 +6,7 @@ class SearchPresenter
   def words
     conn = Faraday.new(url: "https://od-api.oxforddictionaries.com") do |faraday|
       faraday.headers["app_id"] = ENV['APP_ID']
-      faraday.headers["app_key"] = ENV['APP-KEY']
+      faraday.headers["app_key"] = ENV['APP_KEY']
       faraday.adapter Faraday.default_adapter
     end
 
