99335e8f0587c404c4a5897d9375b16f6176377a
ClaireBvs <clairembeauvais@gmail.com>
2018-09-12 11:39:36 -0600

    refactor test for rendering score game

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::GamesController < ApplicationController
 
   def show
+    require "pry"; binding.pry
     render json: Game.find(params[:id]).plays
   end
 end
--- spec/requests/api/v1/user_can_view_game_score_spec.rb
+++ spec/requests/api/v1/user_can_view_game_score_spec.rb
@@ -4,13 +4,12 @@ describe 'GET request to “/api/v1/games/1”' do
   it 'returns a JSON response with game params and score' do
     user = create(:user)
     game = create(:game, id: 1)
-    plays = create(:play)
+    plays = create(:play, game: game)
 
 
     get "/api/v1/games/1"
 
     expect(response.status).to eq 200
-
     game_score = JSON.parse(response.body, symbolize_names: true)
     expect(game_score[:game_id]).to eq(game.id)
   end
