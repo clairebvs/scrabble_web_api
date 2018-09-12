3ec86f82f5a35b20dd9ee9ed19ee1f4e6941aa6f
ClaireBvs <clairembeauvais@gmail.com>
2018-09-12 09:47:20 -0600

    add call api to method words in SearchPresenter

--- app/presenter/search_presenter.rb
+++ app/presenter/search_presenter.rb
@@ -4,6 +4,16 @@ class SearchPresenter
   end
 
   def words
-  end
+    conn = Faraday.new(url: "https://od-api.oxforddictionaries.com") do |faraday|
+      faraday.headers["app_id"] = ENV['APP_ID']
+      faraday.headers["app_key"] = ENV['APP-KEY']
+      faraday.adapter Faraday.default_adapter
+    end
 
+    response = conn.get("/api/v1/inflections/en/foxes")
+
+    JSON.parse(response.body, symbolize_names: true)[:results].map do |word_data|
+      Word.new(word_data)
+    end
+  end
 end
